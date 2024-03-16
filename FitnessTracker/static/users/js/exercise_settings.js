3051ad861960aa3796ba0ca6fa3be072efcb8b1f
Sean Norini <141059916+SeanNorini@users.noreply.github.com>
2024-03-16 17:13:22 -0400

    Refactored exercise_settings.js into Manager class

--- FitnessTracker/static/users/js/exercise_settings.js
+++ FitnessTracker/static/users/js/exercise_settings.js
@@ -1,127 +1,110 @@
-function loadExerciseSettings() {
-  fetch(`http://${domain}/workout/exercise_settings/`, {
-    method: "GET",
-    headers: {
-      Fetch: "True",
-    },
-  })
-    .then((response) => response.text())
-    .then((contentHTML) => {
-      const settingsContent = document.querySelector(".settings");
-      settingsContent.innerHTML = contentHTML;
-      loadExerciseSettingsEventListeners();
-    });
-}
-
-function fetchExercise() {
-  const selectMenu = document.getElementById("select_exercise");
-  const exerciseName = selectMenu.value;
-  fetch(
-    `http://${domain}/workout/exercise_settings/edit_exercise/${exerciseName}`,
-    {
-      method: "GET",
-      headers: {
-        Fetch: "True",
-      },
-    },
-  )
-    .then((response) => response.text())
-    .then((contentHTML) => {
-      const exerciseContainer = document.querySelector(".exercises");
-      exerciseContainer.innerHTML = contentHTML;
-
-      const messageContainer = document.getElementById("message");
-      messageContainer.style.display = "none";
-    });
-}
-
-function addExerciseSelectMenuListener() {
-  const selectMenu = document.getElementById("select_exercise");
-  selectMenu.addEventListener("change", (e) => {
-    fetchExercise();
-  });
-}
-
-function loadExerciseSettingsEventListeners() {
-  addExerciseSelectMenuListener();
-  addNewExerciseListener();
-  addSaveExerciseListener();
-}
-
-function addNewExerciseListener() {
-  document
-    .getElementById("new_exercise")
-    .addEventListener("click", function () {
-      // Prompt for a name
-      const exerciseName = prompt("Please enter name of exercise:");
-
-      // Check if a name was entered
-      if (exerciseName) {
-        // Create a new option element
-        const newOption = document.createElement("option");
-
-        // Set the text content of the option to the entered name
-        newOption.textContent = exerciseName;
-
-        // Add the option to the select menu
-        const selectMenu = document.getElementById("select_exercise");
-        selectMenu.appendChild(newOption);
-        newOption.selected = true;
-        fetchExercise();
-      }
-    });
-}
-
-function saveExerciseSettings() {
-  const formData = new FormData();
-  const fiveRepMax = document.getElementById("five_rep_max");
-  const defaultWeight = document.getElementById("default_weight");
-  const defaultReps = document.getElementById("default_reps");
-  const exerciseName = document.getElementById("select_exercise").value;
-  const csrftoken = document.querySelector("[name=csrfmiddlewaretoken]").value;
-
-  formData.append("csrfmiddlewaretoken", csrftoken);
-  formData.append("five_rep_max", fiveRepMax.value);
-  formData.append("default_weight", defaultWeight.value);
-  formData.append("default_reps", defaultReps.value);
-  formData.append("name", exerciseName);
-
-  fetch(
-    `http://${domain}/workout/exercise_settings/edit_exercise/${exerciseName}`,
-    {
-      method: "POST",
-      body: formData,
-    },
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      if (data.success === true) {
-        showPopupExerciseSaved("Exercise Saved.", 3000);
-      }
-    });
-}
-
-function addSaveExerciseListener() {
-  const saveExerciseButton = document.getElementById("save_exercise");
-  saveExerciseButton.addEventListener("click", (e) => {
-    saveExerciseSettings();
-  });
-}
-
-function showPopupExerciseSaved(message, duration) {
-  // Create a new popup element
-  const popup = document.getElementById("popup");
-  popup.textContent = message;
-  popup.style.display = "block";
-
-  // Automatically close the popup after the specified duration
-  setTimeout(function () {
-    popup.style.display = "none";
-  }, duration);
-}
-
 class ExerciseSettingsManager {
-  initialize() {}
-}
+  initialize() {
+    pageManager
+      .fetchData({
+        url: `${pageManager.baseURL}/workout/exercise_settings/`,
+        method: "GET",
+        responseType: "text",
+      })
+      .then((contentHTML) => {
+        pageManager.updateContent(contentHTML, "settings");
+        this.loadExerciseSettingsEventListeners();
+      });
+  }
 
+  loadExerciseSettingsEventListeners() {
+    this.addExerciseSelectMenuListener();
+    this.addNewExerciseListener();
+    this.addSaveExerciseListener();
+  }
+
+  addNewExerciseListener() {
+    document.getElementById("new_exercise").addEventListener("click", (e) => {
+      this.addNewExercise();
+    });
+  }
+
+  addNewExercise() {
+    const exerciseName = prompt("Please enter name of exercise:");
+
+    // Check if a name was entered
+    if (exerciseName) {
+      // Create a new option element
+      const newOption = document.createElement("option");
+
+      // Set the text content of the option to the entered name
+      newOption.textContent = exerciseName;
+
+      // Add the option to the select menu
+      const selectMenu = document.getElementById("select_exercise");
+      selectMenu.appendChild(newOption);
+      newOption.selected = true;
+      this.fetchExercise();
+    }
+  }
+
+  addSaveExerciseListener() {
+    const saveExerciseButton = document.getElementById("save_exercise");
+    saveExerciseButton.addEventListener("click", (e) => {
+      this.saveExerciseSettings();
+    });
+  }
+
+  readExerciseSettings() {
+    const formData = new FormData();
+    const fiveRepMax = document.getElementById("five_rep_max");
+    const defaultWeight = document.getElementById("default_weight");
+    const defaultReps = document.getElementById("default_reps");
+    const exerciseName = document.getElementById("select_exercise").value;
+    const csrftoken = document.querySelector(
+      "[name=csrfmiddlewaretoken]",
+    ).value;
+
+    formData.append("csrfmiddlewaretoken", csrftoken);
+    formData.append("five_rep_max", fiveRepMax.value);
+    formData.append("default_weight", defaultWeight.value);
+    formData.append("default_reps", defaultReps.value);
+    formData.append("name", exerciseName);
+
+    return formData;
+  }
+
+  saveExerciseSettings() {
+    const formData = this.readExerciseSettings();
+    pageManager
+      .fetchData({
+        url: `${pageManager.baseURL}/workout/exercise_settings/edit_exercise/${formData.get("exerciseName")}`,
+        method: "POST",
+        body: formData,
+        responseType: "json",
+      })
+      .then((response) => {
+        if (response.success === true) {
+          pageManager.showTempPopupMessage("Exercise Saved.", 2000);
+        }
+      });
+  }
+
+  addExerciseSelectMenuListener() {
+    const selectMenu = document.getElementById("select_exercise");
+    selectMenu.addEventListener("change", (e) => {
+      this.fetchExercise();
+    });
+  }
+
+  fetchExercise() {
+    const selectMenu = document.getElementById("select_exercise");
+    const exerciseName = selectMenu.value;
+
+    pageManager
+      .fetchData({
+        url: `${pageManager.baseURL}/workout/exercise_settings/edit_exercise/${exerciseName}`,
+        method: "GET",
+        responseType: "text",
+      })
+      .then((contentHTML) => {
+        pageManager.updateContent(contentHTML, "exercise");
+      });
+  }
+}
 const exerciseSettingsManager = new ExerciseSettingsManager();
